b2bc6f9fcc00e4cdb8d7d7f269ce3bf0b28eab60
AMH <championboylay@gmail.com>
2017-02-04 20:11:36 -0800

    Added bootstrap and jquery dependencies to pom

---
+++ src/main/java/com/devoppsbuddy/DevoppsbuddyApplication.java
@@ -0,0 +1,12 @@
+package com.devoppsbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevoppsbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevoppsbuddyApplication.class, args);
+	}
+}
---
+++ src/main/java/com/devoppsbuddy/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.devoppsbuddy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/hello")
+	public String helloWorld(){
+		return "index";
+	}
+}
