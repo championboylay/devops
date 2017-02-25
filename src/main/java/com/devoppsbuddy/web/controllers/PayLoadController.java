fee2432cb156005da46c7e023918a19b48cde73f
AMH <championboylay@gmail.com>
2017-02-25 14:24:16 -0800

    Addd spring security

--- src/main/java/com/devoppsbuddy/web/controllers/PayLoadController.java
+++ src/main/java/com/devoppsbuddy/web/controllers/PayLoadController.java
@@ -1,11 +1,13 @@
 package com.devoppsbuddy.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PayLoadController {
 
 	private final String PAYLOAD_VIEW_NAME="payload/payload";
+	@RequestMapping("/payload")
 	public String payload(){
 		return PAYLOAD_VIEW_NAME;
 	}
